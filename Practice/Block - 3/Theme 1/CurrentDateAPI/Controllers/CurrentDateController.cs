fe742d2ea64ef514e848ad50b7e7117c4d339257
mxwrlld <maksim.mixwell@gmail.com>
2021-12-18 11:38:26 +0300

    Добавление первого задания

---
+++ Practice/Block - 3/Theme 1/CurrentDateAPI/Controllers/CurrentDateController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrentDateAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CurrentDateController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<DateTime> Get()
+        {
+            return DateTime.Now;
+        }
+
+        [Route("date")]
+        [HttpGet]
+        public ActionResult<string> GetDateWithFormat(string format = "D")
+        {
+            try
+            {
+                string date = DateTime.Now.Date.ToString(format);
+                return date;
+            }
+            catch(FormatException ex)
+            {
+                return String.Format("Введён неверный формат даты");
+            }
+        }
+
+        
+        [Route("time")]
+        [HttpGet]
+        public ActionResult<string> GetTime()
+        {
+            return DateTime.Now.ToString("T");
+        }
+    }
+}
